276471b77d07550d0b7812507f011bcc6eebc0fe
Eka <eka.abashidze@gmail.com>
2023-08-21 19:30:00 +0400

    refactor: get back the whole code after making a tutorial

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,28 +1,74 @@
 <template>
   <div
+    draggable="true"
+    @dragstart="handleDragStart"
+    @dragover="handleDragOver"
+    @dragend="handleDragEnd"
+    :class="{ 'border-b-[#4D5067]': dark }"
     class="border-b border-b-[#E3E4F1] h-12 flex items-center gap-x-3 justify-between cursor-pointer"
   >
     <div class="flex items-center gap-x-3">
-      <button class="relative cursor-pointer">
+      <button class="relative cursor-pointer" @click="toggleActive">
         <div
+          :class="{
+            'bg-gradient-to-br from-[#55DDFF] to-[#C058F3]': !item.active,
+            'border-[#4D5067]': dark,
+          }"
           class="w-[20px] h-[20px] border border-[#E3E4F1] rounded-full"
         ></div>
         <img
+          v-if="!item.active"
           src="../assets/icons/icon-check.svg"
           alt=""
           class="absolute left-[4.5px] top-[6px]"
         />
       </button>
-      <p class="font-normal text-xs">TODO</p>
+      <p
+        :class="{
+          'text-[#494C6B]': item.active,
+          'text-[#D1D2DA]  line-through': !item.active,
+          'text-[#C8CBE7]': dark,
+          'text-[#4D5067]': dark && !item.active,
+        }"
+        class="font-normal text-xs"
+      >
+        {{ item.text }}
+      </p>
     </div>
     <img
       src="../assets/icons/icon-cross.svg"
       alt=""
       class="w-3 h-3 cursor-pointer"
+      @click="$emit('remove-item')"
     />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+    dark: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    toggleActive() {
+      this.$emit("toggle-active", !this.item.active);
+    },
+    handleDragStart(event) {
+      this.$emit("dragstart", this.item, this.index);
+    },
+    handleDragOver(event) {
+      this.$emit("dragover", this.index);
+    },
+    handleDragEnd() {
+      this.$emit("dragend");
+    },
+  },
+};
 </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,10 @@
 <template>
   <div
-    class="h-screen bg-no-repeat bg-contain flex flex-col items-center bg-[#FAFAFA] bg-mobile-light sm:bg-desktop-light"
+    :zclass="{
+      'bg-[#FAFAFA] bg-mobile-light sm:bg-desktop-light': !isDarkMode,
+      'bg-[#171823] bg-mobile-dark sm:bg-desktop-dark': isDarkMode,
+    }"
+    class="h-screen bg-no-repeat bg-contain flex flex-col items-center"
   >
     <div
       class="flex justify-between pt-12 px-6 w-full sm:max-w-[540px] sm:px-0"
@@ -11,49 +15,111 @@
         class="max-w-[109px] max-h-[20px]"
       />
       <img
+        v-if="!isDarkMode"
         src="../assets/icons/icon-moon.svg"
         alt=""
         class="max-w-[20px] max-h-[20px] cursor-pointer"
+        @click="toggleDarkMode"
+      />
+      <img
+        v-if="isDarkMode"
+        src="../assets/icons/icon-sun.svg"
+        alt=""
+        class="max-w-[20px] max-h-[20px] cursor-pointer"
+        @click="toggleDarkMode"
       />
     </div>
 
     <div class="mt-10 mx-6 relative">
       <div
+        :class="{ 'border-[#4D5067]': isDarkMode }"
         class="w-[20px] h-[20px] border border-[#E3E4F1] absolute top-[12px] left-5 rounded-full"
       ></div>
 
       <input
         type="text"
         placeholder="Create a new todo..."
-        class="w-[327px] sm:w-[540px] h-12 rounded-lg shadow-md px-14 text-xs focus:outline-none bg-white"
+        :class="{
+          'bg-[#25273D] text-[#C8CBE7] placeholder:text-[#767992]': isDarkMode,
+          'bg-white': !isDarkMode,
+        }"
+        class="w-[327px] sm:w-[540px] h-12 rounded-lg shadow-md px-14 text-xs focus:outline-none"
+        v-model="newItem"
+        @keyup.enter="addItem"
       />
     </div>
 
-    <div class="px-5 mt-4 rounded-lg shadow-md w-[327px] sm:w-[540px] bg-white">
-      <div>
-        <todo-item></todo-item>
+    <div
+      :class="{
+        'bg-[#25273D] text-[#C8CBE7]': isDarkMode,
+        'bg-white': !isDarkMode,
+      }"
+      class="px-5 mt-4 rounded-lg shadow-md w-[327px] sm:w-[540px]"
+    >
+      <div
+        v-for="(item, index) in filterList"
+        :key="index"
+        @dragstart="handleDragStart(item, index)"
+        @dragover="handleDragOver(index)"
+        @dragend="handleDragEnd"
+      >
+        <todo-item
+          :item="item"
+          :active="item.active"
+          :dark="isDarkMode"
+          @remove-item="removeItem(index)"
+          @toggle-active="toggleActive(index)"
+          :class="{
+            dragging: draggingIndex === index,
+            'drag-over': dragOverIndex === index,
+          }"
+        ></todo-item>
       </div>
 
       <div
+        :class="{ 'text-[#5B5E7E]': isDarkMode }"
         class="flex justify-between pt-4 pb-[22px] font-normal text-[#9495A5] text-xs"
       >
-        <p>items left</p>
-        <p>completed</p>
+        <p>{{ activeCount }} items left</p>
+        <p>{{ inactiveCount }} completed</p>
       </div>
     </div>
 
     <div
-      class="w-[327px] sm:w-[540px] h-12 rounded-lg shadow-md mt-4 mx-6 flex justify-around items-center font-bold text-[#9495A5] text-sm px-[70px] bg-white"
+      :class="{
+        'bg-[#25273D] text-[#5B5E7E]': isDarkMode,
+        'bg-white': !isDarkMode,
+      }"
+      class="w-[327px] sm:w-[540px] h-12 rounded-lg shadow-md mt-4 mx-6 flex justify-around items-center font-bold text-[#9495A5] text-sm px-[70px]"
     >
-      <button class="cursor-pointer">All</button>
-      <button class="cursor-pointer">Active</button>
-      <button class="cursor-pointer">Completed</button>
+      <button
+        @click="modifyList('All')"
+        :class="{ 'text-[#3A7CFD]': listType === 'All' }"
+        class="cursor-pointer"
+      >
+        All
+      </button>
+      <button
+        @click="modifyList('Active')"
+        :class="{ 'text-[#3A7CFD]': listType === 'Active' }"
+        class="cursor-pointer"
+      >
+        Active
+      </button>
+      <button
+        @click="modifyList('Completed')"
+        :class="{ 'text-[#3A7CFD]': listType === 'Completed' }"
+        class="cursor-pointer"
+      >
+        Completed
+      </button>
       <button class="hidden sm:block" @click="clearCompleted()">
         Clear completed
       </button>
     </div>
 
     <div
+      :class="{ 'text-[#5B5E7E]': isDarkMode }"
       class="mt-10 mb-10 flex justify-center font-normal text-[#9495A5] text-sm"
     >
       <p>Drag and drop to reorder list</p>
@@ -69,11 +135,82 @@ export default {
     TodoItem,
   },
   data() {
-    return {};
+    return {
+      newItem: "",
+      list: [
+        { text: "Jog around the park 5x", active: true },
+        { text: "10 minutes meditation", active: true },
+        { text: "Read for an hour", active: true },
+      ],
+      listType: "All",
+      draggingIndex: null,
+      dragOverIndex: null,
+      isDarkMode: false,
+    };
   },
-  computed: {},
-  methods: {},
+  computed: {
+    inactiveCount() {
+      return this.list.filter((item) => !item.active).length;
+    },
+    activeCount() {
+      return this.list.filter((item) => item.active).length;
+    },
+    filterList() {
+      if (this.listType === "Active") {
+        return this.list.filter((item) => item.active);
+      } else if (this.listType === "Completed") {
+        return this.list.filter((item) => !item.active);
+      } else {
+        return this.list;
+      }
+    },
+  },
+  methods: {
+    addItem() {
+      this.list.unshift({ text: this.newItem, active: true });
+      this.newItem = "";
+    },
+    removeItem(index) {
+      this.list.splice(index, 1);
+    },
+    toggleActive(index) {
+      this.list[index].active = !this.list[index].active;
+    },
+    modifyList(type) {
+      this.listType = type;
+      console.log(this.listType);
+    },
+    handleDragStart(item, index) {
+      this.draggingIndex = index;
+    },
+    handleDragOver(index) {
+      this.dragOverIndex = index;
+    },
+    handleDragEnd() {
+      if (this.draggingIndex !== null && this.dragOverIndex !== null) {
+        const draggedItem = this.list.splice(this.draggingIndex, 1)[0];
+        this.list.splice(this.dragOverIndex, 0, draggedItem);
+      }
+      this.draggingIndex = null;
+      this.dragOverIndex = null;
+    },
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
+    },
+    clearCompleted() {
+      this.list = this.list.filter((item) => item.active);
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+.dragging {
+  opacity: 0.2;
+}
+
+.drag-over {
+  border-top: 1px dashed #3a7cfd;
+  border-bottom: 1px dashed #3a7cfd;
+}
+</style>
